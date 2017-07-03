9b361445ba3b24d30756da47084cefa5ea59b143
SaushkinDenis <saushkinden@mail.ru>
2017-07-03 17:54:45 +0400

    Creator

--- Generator/src/generator/Generator.java
+++ Generator/src/generator/Generator.java
@@ -1,46 +1,23 @@
 package generator;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.Properties;
-import java.util.Scanner;
-
+import static generator.Creator.createXMLFile;
+import static generator.Creator.readLine;
 
 public class Generator {
-
-    public static String[] readLine(){
-        Scanner scanner = new Scanner(System.in, "CP1251"); //"CP1251","utf-8"
-        System.out.print("Enter the operation: ");
-        String text = scanner.nextLine();
-        String[] cop = text.split(",");
-        return cop;
+    
+    public static void General(){
+        String[] cop = readLine();
+        String text = WorkWithJson(cop);
+        createXMLFile(text);
     }
 
-    private static String brackets(String bracket){
-        String param = bracket.substring(1,bracket.length()-1);
-        return param;
-    }
-
-    public static void createXMLFile(String text2){
-
-        File dir = new File("C://Users//usersbt//Downloads//File.xsl");
-        File file = new File(dir, "File.xsl");
-        try(FileWriter writer = new FileWriter(dir, false))
-        {
-            String text = "<xsl:stylesheet version=\"1.0\" xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\"> \r\n";
-            writer.write(text);
-            writer.write(text2);
-            System.out.print(text + text2);
-            writer.flush();
-        }
-        catch(IOException ex){  System.out.println(ex.getMessage());}
-    }
     public static final String PATH_TO_PROPERTIES = "src/resources/config.properties";
 
-    public static String choice(String...cop){
+    public static String WorkWithJson(String...cop){
         FileInputStream fileInputStream;
 
         Properties prop = new Properties();
@@ -50,17 +27,17 @@ public class Generator {
             fileInputStream = new FileInputStream(PATH_TO_PROPERTIES);
             Charset.forName("UTF-8");
             prop.load(fileInputStream);
+            
             String op = cop[0];
             
             text = prop.getProperty(op);
-
+            
             
         } catch (IOException e) {
             System.out.println("Ошибка в программе: файл " + PATH_TO_PROPERTIES + " не обнаружено");
             e.printStackTrace();
         }
         return text;
-
 
 /*
         switch(cop[0]){
@@ -96,6 +73,13 @@ public class Generator {
 */
     }
 
+    
+    
+      private static String brackets(String bracket){
+        String param = bracket.substring(1,bracket.length()-1);
+        return param;
+    }
+      
     private static String loop(String...value){
         String text = ""
                 + "<xsl:for-each select = “"+value[0]+"” > \r\n" +
