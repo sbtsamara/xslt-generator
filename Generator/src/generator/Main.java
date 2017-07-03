1517c96825f17ec6ca2e122335d7cb453f108a17
SaushkinDenis <saushkinden@mail.ru>
2017-07-03 17:54:45 +0400

    Creator

--- Generator/src/generator/Main.java
+++ Generator/src/generator/Main.java
@@ -1,14 +1,9 @@
 package generator;
-
-import static generator.Generator.choice;
-import static generator.Generator.createXMLFile;
-import static generator.Generator.readLine;
+import static generator.Generator.General;
 
 public class Main {
     
     public static void main(String[] args) {
-        String[] cop = readLine();
-        String text = choice(cop); 
-        createXMLFile(text);
+        General();
     }
 }
