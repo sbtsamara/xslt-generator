9b3f497910b118270198d6762545660f055d31c5
SaushkinDenis <saushkinden@mail.ru>
2017-07-05 14:24:16 +0400

    Create README.md

--- Generator/src/generator/WorkJson.java
+++ Generator/src/generator/WorkJson.java
@@ -1,4 +1,3 @@
-
 package generator;
 import java.io.FileReader;
 import java.io.IOException;
@@ -11,7 +10,6 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-
 public class WorkJson {
    
     private static final String FILENAME = "C:\\Users\\usersbt\\Documents\\NetBeansProjects\\xslt-generator\\Generator\\src\\generator\\Json.json";
@@ -22,21 +20,17 @@ public class WorkJson {
         ArrayList[] operations = null;
         try {
             JSONObject object = (JSONObject) parser.parse(new FileReader(FILENAME));            
-            operation = (String) object.get(attribute[0]);
-            
+            operation = (String) object.get(attribute[0]);            
             for(int i=1; i < attribute.length; i++){
-                operation = operation.replaceFirst("\\?",brackets(attribute[i]));
-                
+                operation = operation.replaceFirst("\\?",brackets(attribute[i]));               
             }
             
-           
- 
         } catch (IOException | ParseException ex) {
             Logger.getLogger(WorkJson.class.getName()).log(Level.SEVERE, null, ex);
         }
          return operation;
     }
-
+    
     private static String brackets(String bracket){
         String param = bracket.substring(1,bracket.length()-1);
         return param;
